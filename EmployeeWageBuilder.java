d5b3e66bc6219f43cf911275a8a59a2f311b8ff2
souvik1232 <souvik199831@gmail.com>
2020-12-14 22:04:32 +0530

    Added:Logic for employee attendance

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,5 +3,11 @@ package com.java.empwage;
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
