5977ee768a409b67512de33b027378ba8b060814
souvik1232 <souvik199831@gmail.com>
2020-12-14 22:31:57 +0530

    Refractotred:Logic to satisfy maximum hours and days condition

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,23 +4,31 @@ public class EmployeeWageBuilder {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HOURS_IN_MONTH = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        int empHrs = 0;
-        int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        switch ((int)empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+        int totalEmpWage = 0, totalWorkingDays = 0,totalEmpHrs = 0;
+        while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            int empHrs = 0;
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Emp Wage : " + empWage);
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage : " + empWage);
+        System.out.println("Total Emp Wage : " + totalEmpWage);
     }
 }
