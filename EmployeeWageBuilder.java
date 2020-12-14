2230680cab415c100b6a23b93bd22e424fce7199
souvik1232 <souvik199831@gmail.com>
2020-12-14 22:02:34 +0530

    Added:Welcome message

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,7 @@
+package com.java.empwage;
+
+public class EmployeeWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
