ea810953b049b98b58be20acd44d04ea38119624
souvik1232 <souvik199831@gmail.com>
2020-12-14 22:12:50 +0530

    Added:Logic for daily employee wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,10 +4,13 @@ public class EmployeeWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+            empHrs = 8;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage : " + empWage);
     }
 }
